7bc8cf5917035095a2fc609654cda7f1d7c02b45
10h-pack <345628400@qq.com>
2025-01-15 22:58:47 +0800

    Add files via upload

---
+++ whitelist.lua
@@ -0,0 +1,25 @@
+local StarterGui = game:GetService("StarterGui")
+local Players = game:GetService("Players")
+local WhitelistedPlayers = {
+    ["10h_pack"] = true,
+}
+
+local function IsWhitelisted(player)
+    return WhitelistedPlayers[player.Name] or false
+end
+
+local localPlayer = Players.LocalPlayer
+
+local isLocalPlayerWhitelisted = IsWhitelisted(localPlayer)
+
+if isLocalPlayerWhitelisted then
+    StarterGui:SetCore("SendNotification", {
+        Title = "白名单认证",
+        Text = "玩家:"..localPlayer.Name.."，检测完毕白名单玩家，稍后会加载脚本",
+        Duration = 7,
+    })
+    loadstring(game:HttpGet(""))()
+    -- 在这里添加你的脚本不是源码
+else
+    localPlayer:Kick("bro")
+end
