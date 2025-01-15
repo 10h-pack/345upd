c946e4871cb1bc2a47fcf7b42d112f5e9dd186e9
10h-pack <345628400@qq.com>
2025-01-16 01:03:53 +0800

    Update 345.lua

--- 345.lua
+++ 345.lua
@@ -43,9 +43,149 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/VIP/main/%E
 end
 })   
 local Tab = Window:MakeTab({
-    Name = "制作中...",
+    Name = "通用",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
+Tab:AddButton({
+	Name = "飞行",
+	Callback = function()
+loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\34\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\83\76\120\100\97\75\112\119\34\41\40\41\10")()
+end
+})    
+Tab:AddButton({
+	Name = "死亡笔记",
+	Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/krlpl/dfhj/main/%E6%AD%BB%E4%BA%A1%E7%AC%94%E8%AE%B0.txt"))()
+end
+})    
+Tab:AddButton({
+	Name = "子追",
+	Callback = function()
+loadstring(game:HttpGet("https://pastebin.com/raw/1AJ69eRG"))()
+end
+})  
+Tab:AddButton({
+	Name = "飞车",
+	Callback = function()
+loadstring(game:HttpGet("https://pastebin.com/raw/63T0fkBm"))()
+end
+})    
+Tab:AddButton({
+	Name = "无限跳跃",
+	Callback = function()
+loadstring(game:HttpGet("https://pastebin.com/raw/V5PQy3y0", true))()
+end
+})    
+Tab:AddButton({
+	Name = "自杀",
+	Callback = function()
+game.Players.LocalPlayer.Character.Humanoid.Health=0
+end
+})    
+Tab:AddButton({
+	Name = "Anti-AFK",
+	Callback = function()
+loadstring(game:HttpGet("https://pastebin.com/raw/9fFu43FF"))()
+end
+})    
+Tab:AddButton({
+	Name = "键盘",
+	Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt"))()
+end
+})    
+Tab:AddButton({
+	Name = "*透视*",
+	Callback = function()
+loadstring(game:HttpGet("https://pastebin.com/raw/uw2P2fbY"))()
+end
+})    
+Tab:AddSlider({
+
+	Name = "跳跃高度",
+
+	Min = 50,
+
+	Max = 200,
+
+	Default = 50,
+
+	Color = Color3.fromRGB(255,255,255),
+
+	Increment = 1,
+
+	ValueName = "数值",
+
+	Callback = function(Value)
+
+		game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+
+	end    
+
+})
+Tab:AddTextbox({
+
+	Name = "移动速度设置",
+
+	Default = "",
+
+	TextDisappear = true,
+
+	Callback = function(Value)
+
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+		
+		OrionLib:MakeNotification({
+                    Name = "成功设置速度:  "..game.Players.LocalPlayer.Character.Humanoid.WalkSpeed.."   |速度",
+                    Content = "成功",
+                    Time = 3
+                })
+	end
+})
+Tab:AddTextbox({
+	Name = "重力设置",
+	Default = "",
+	TextDisappear = true,
+	Callback = function(Value)
+		game.Workspace.Gravity = Value
+		
+		OrionLib:MakeNotification({
+                    Name = "成功设置重力:  "..game.Workspace.Gravity.."   |重力",
+                    Content = "成功",
+                    Time = 3
+                })
+	end
+})
+Tab:AddTextbox({
+	Name = "跳跃高度设置",
+	Default = "",
+	TextDisappear = true,
+	Callback = function(Value)
+		game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+		
+		OrionLib:MakeNotification({
+                    Name = "成功设置跳跃高度:  "..game.Players.LocalPlayer.Character.Humanoid.WalkSpeed.."   |高度",
+                    Content = "成功",
+                    Time = 3
+                })
+	end
+})
 
 
+Tab:AddToggle({
+	Name = "夜视",
+	Default = false,
+	Callback = function(Value)
+		if Value then
+
+		    game.Lighting.Ambient = Color3.new(1, 1, 1)
+
+		else
+
+		    game.Lighting.Ambient = Color3.new(0, 0, 0)		    		    
+
+		end
+
+	end	
+})
