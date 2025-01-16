8c793eccacaabd31e87c544fb4794699e633ef67
10h-pack <345628400@qq.com>
2025-01-16 18:21:16 +0800

    Update 345.lua

--- 345.lua
+++ 345.lua
@@ -184,7 +184,13 @@ Tab:AddButton({
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
 end
 })    
- 
+Tab:AddButton({
+	Name = "光影",
+	Callback = function()
+loadstring(game:HttpGet("https://pastebin.com/raw/jHBfJYmS"))()
+end
+})    
+
 
 local Tab = Window:MakeTab({
     Name = "dex和spy",
@@ -243,5 +249,54 @@ Tab:AddButton({
 	Name = "夜视仪",
 	Callback = function()
 loadstring(game:HttpGet("https://pastebin.com/raw/4Vsv1Xwn"))()
+end
+})    
+local Tab = Window:MakeTab({
+    Name = "内脏与黑火药",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "1",
+	Callback = function()
+loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\67\104\105\110\97\81\89\47\83\99\114\105\112\116\115\47\77\97\105\110\47\71\66"))()
+end
+})    
+local Tab = Window:MakeTab({
+    Name = "极速传奇",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "1",
+	Callback = function()
+loadstring(game:HttpGet('\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\98\111\121\115\99\112\47\98\101\116\97\47\109\97\105\110\47\37\69\57\37\56\48\37\57\70\37\69\53\37\66\65\37\65\54\37\69\55\37\56\50\37\66\56\37\69\56\37\66\53\37\66\55\46\108\117\97'))()
+end
+		local Tab = Window:MakeTab({
+    Name = "造船寻宝",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+		Tab:AddButton({
+	Name = "刷钱",
+	Callback = function()
+loadstring(game:HttpGet("https://pastebin.com/raw/Lyy77rnr",true))()
+end
+})    
+		local Tab = Window:MakeTab({
+    Name = "evade",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+		Tab:AddButton({
+	Name = "1",
+	Callback = function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/9Strew/roblox/main/gamescripts/evade.lua'))()
+end
+})    
+		Tab:AddButton({
+	Name = "2",
+	Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingScripter/Darkrai-X/main/Games/Evade"))()
 end
 })    
