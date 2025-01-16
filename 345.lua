ed19112af636a62826fdaeaecff644d70c57d6cf
10h-pack <345628400@qq.com>
2025-01-16 18:27:17 +0800

    Update 345.lua

--- 345.lua
+++ 345.lua
@@ -87,7 +87,30 @@ end
 Tab:AddButton({
 	Name = "*透视*",
 	Callback = function()
-loadstring(game:HttpGet("https://pastebin.com/raw/uw2P2fbY"))()
+local function ApplyESP(v)
+   if v.Character and v.Character:FindFirstChildOfClass'Humanoid' then
+       v.Character.Humanoid.NameDisplayDistance = 9e9
+       v.Character.Humanoid.NameOcclusion = "NoOcclusion"
+       v.Character.Humanoid.HealthDisplayDistance = 9e9
+       v.Character.Humanoid.HealthDisplayType = "AlwaysOn"
+       v.Character.Humanoid.Health = v.Character.Humanoid.Health -- triggers changed
+   end
+end
+for i,v in pairs(game.Players:GetPlayers()) do
+   ApplyESP(v)
+   v.CharacterAdded:Connect(function()
+       task.wait(0.33)
+       ApplyESP(v)
+   end)
+end
+
+game.Players.PlayerAdded:Connect(function(v)
+   ApplyESP(v)
+   v.CharacterAdded:Connect(function()
+       task.wait(0.33)
+       ApplyESP(v)
+   end)
+end)
 end
 })    
 Tab:AddSlider({
@@ -190,6 +213,7 @@ Tab:AddButton({
 loadstring(game:HttpGet("https://pastebin.com/raw/jHBfJYmS"))()
 end
 })    
+
 
 
 local Tab = Window:MakeTab({
